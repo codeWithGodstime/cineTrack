e1247ac14ee3e7aa8c0e678b11ca00a8fd9cc3c7
codeWithGodstime <specialgodstym@gmail.com>
2025-06-06 00:24:58 +0100

    display trending tv and movies

--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.WatchlistView.as_view(), name='watchlist'),
     path('mark-watching/<int:item_id>/', views.mark_as_watching, name='mark_watching'),
     path('mark-completed/<int:item_id>/', views.mark_as_completed, name='mark_completed'),
+    path('trending/', views.TrendingView.as_view(), name='trending'),
+    path('add-to-watchlist/', views.add_to_watchlist, name='add_to_watchlist'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,11 +1,16 @@
+import requests
+import json
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import View
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.conf import settings
+
 
 from .models import Watchlist
 
-class WatchlistView(View):
+class WatchlistView(LoginRequiredMixin, View):
     def get(self, request):
         watchlist = Watchlist.objects.filter(user=request.user).order_by('-added_on')
         completed = Watchlist.objects.filter(user=request.user, is_completed=True).order_by('-updated_at')
@@ -36,3 +41,46 @@ def mark_as_completed(request, item_id):
         item.save()
         messages.success(request, f'Congratulations! You completed "{item.title}"!')
     return redirect('watchlist')
+
+
+class TrendingView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        api_key = settings.TMDB_API_KEY
+        type = self.request.GET.get("type") if self.request.GET.get('type') else 'movie'
+        time_window = self.request.GET.get('time_window') if self.request.GET.get('time_window') else 'week'
+        
+        url = f'https://api.themoviedb.org/3/trending/{type}/{time_window}?api_key={api_key}'
+        headers = {
+            "accept": "application/json",
+            "Authorization": f"Bearer {api_key}"
+        }
+
+        response = requests.get(url, headers=headers)
+        data = response.json()
+        template = 'core/trending-movie.html' if type == 'movie' else 'core/trending-tv.html'
+        movies = data['results']
+        return render(request, template, {'movies': movies})
+
+@login_required
+def add_to_watchlist(request):
+    if request.method == 'POST':
+        tmdb_id = request.POST.get('tmdb_id')
+        title = request.POST.get('title')
+        media_type = request.POST.get('media_type')
+        poster_path = request.POST.get('poster_path')
+
+        # Check if already in watchlist
+        if not Watchlist.objects.filter(user=request.user, tmdb_id=tmdb_id, media_type=media_type).exists():
+            # Create new watchlist item
+            Watchlist.objects.create(
+                user=request.user,
+                tmdb_id=tmdb_id,
+                title=title,
+                media_type=media_type,
+                thumbnail=f'https://image.tmdb.org/t/p/w500{poster_path}'
+            )
+            messages.success(request, f'"{title}" added to your watchlist!')
+        else:
+            messages.info(request, f'"{title}" is already in your watchlist.')
+
+    return redirect('trending')
