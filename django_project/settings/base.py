d67b2cfff7bd7058fc0de92399286b5d4285bac3
codeWithGodstime <specialgodstym@gmail.com>
2025-06-06 15:54:38 +0100

    fix whitenoise staticfiles

--- django_project/settings/base.py
+++ django_project/settings/base.py
@@ -93,7 +93,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
